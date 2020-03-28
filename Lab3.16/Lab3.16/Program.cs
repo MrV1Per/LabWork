82a852f0c3ef12961cae0963b63b033c2dfdcc20
Vladyslav <vlad.movshan@gmail.com>
2020-03-28 14:39:08 +0200

    Variable register changed, static methods removed

--- Lab3.16/Lab3.16/Program.cs
+++ Lab3.16/Lab3.16/Program.cs
@@ -1,81 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab3._16
 {
     class Program
     {
+
         /// <summary>
         /// To search for the factorial of a given variable
         /// </summary>
-        /// <param name="N"></param>
+        /// <param name="inputNum">The factorial number of which is to be calculated</param>
         /// <returns></returns>
-        private static double fact(int N)
+        private double Fact(int inputNum)
         {
-            if (N < 0)
+            if (inputNum < 0)
                 return 0;
-            if (N == 0)
+            if (inputNum == 0)
                 return 1;
             else
-                return N * fact(N - 1);
+                return inputNum * Fact(inputNum - 1);
         }
-
         /// <summary>
         /// To find Step of increase variable X
         /// </summary>
-        /// <param name="Lover">Lover limit of variable X</param>
-        /// <param name="Upper">Upper limit of variable X</param>
+        /// <param name="lover">Lover limit of variable X</param>
+        /// <param name="upper">Upper limit of variable X</param>
         /// <returns></returns>
-        private static float FindStep(float Lover, int Upper)
+        private float FindStep(float lover, int upper)
         {
-            float Step = (Upper - Lover) / 10;
-            return Step;
+            float step = (upper - lover) / 10;
+            return step;
         }
-
         /// <summary>
         /// To find Sum of given expression
         /// </summary>
-        /// <param name="Lover">Lover limit of variable X</param>
-        /// <param name="Upper">Upper limit of variable X</param>
-        private static void FindSum(float Lover, int Upper)
+        /// <param name="lover">Lover limit of variable X</param>
+        /// <param name="upper">Upper limit of variable X</param>
+        private void FindSum(float lover, int upper)
         {
-            int FormulaN = 10, FormulaK = 1;
-            float FormulaX = Lover, Step = FindStep(Lover, Upper);
-
-            double FinalSum = 0;
+            Program prog = new Program();
+            int formulaN = 10, formulaK = 1;
+            float formulaX = lover, step = prog.FindStep(lover, upper);
+            double finalSum = 0;
            
                  Console.WriteLine($"\n|Number of iteration|         Value         |");
-            while (FormulaN > 0)
+            while (formulaN > 0)
             {
-
-                FinalSum += Math.Pow(-1, FormulaK) * (Math.Pow(1 + FormulaX, 2 * FormulaK) / fact(FormulaK));
-
-                if(FormulaX <= Upper)
+                finalSum += Math.Pow(-1, formulaK) * (Math.Pow(1 + formulaX, 2 * formulaK) / prog.Fact(formulaK));
+                if(formulaX <= upper)
                 {
-                    FormulaX += Step;
+                    formulaX += step;
                 }
-                Console.WriteLine($"|-------------------|-----------------------|\n|  {FormulaK} iteration      -> {FinalSum}");
-                FormulaK++;
-                FormulaN--;
+                Console.WriteLine($"|-------------------|-----------------------|\n|  {formulaK} iteration      -> {finalSum}");
+                formulaK++;
+                formulaN--;
             }
-            Console.WriteLine($"\n\nThe final sum is       -> {FinalSum} \nWithout exponent -> {FinalSum.ToString("F0")}");
-            
-
+            Console.WriteLine($"\n\nThe final sum is  -> {finalSum} \nWithout exponent  -> {finalSum.ToString("F0")}");
         }
         static void Main(string[] args)
         {
-            float LoverLimit = -1.05f;
-            int UpperLimit = 362;
+            Program prog = new Program();
+            float loverLimit = -1.05f;
+            int upperLimit = 362;
 
-            Console.WriteLine($"Hello dear user, this program was created to help you to find sum for the interval \nfrom - {LoverLimit},  to - {UpperLimit}, \nwith step - {FindStep(LoverLimit, UpperLimit)}");
-            FindSum(LoverLimit, UpperLimit);
-
-
+            Console.WriteLine($"Hello dear user, this program was created to help you to find sum for the interval " +
+                $"\nfrom - {loverLimit},  to - {upperLimit}, " +
+                $"\nwith step - {prog.FindStep(loverLimit, upperLimit)}");
+            prog.FindSum(loverLimit, upperLimit);
             Console.ReadKey();
-         
         }
     }
 }
